507f71aa8438a7a8995c3c0f50b2902cf0e0e074
Zairbek <xaiker007@gmail.com>
2018-04-20 01:51:16 +0300

    при нажатие другую местуб меню должно закрыватся

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 window.onload = function () {
-  slideForHide();
+  swipeForShow('+', 'swipeBtnToShow', 'swipeElement','0.3');
 
 }
 
-function slideForHide(){
-  var menuBtnToHide = document.getElementById('menuBtnToHide');
-  menuBtnToHide.addEventListener('click', function(){
-    var menu = document.getElementById('menu');
-  })
+ 
+function getId (idArgument) {                   // Функция для быстрое получение ID   
+  return document.getElementById(idArgument);
+}
+
+
+
+
+function swipeForShow(axicX = '+', swipeBtnToShow, swipeElement, delay = '0.4'){
+  var swipeBtnToShow = getId(swipeBtnToShow);
+  var swipeElement = getId(swipeElement);
+
+  var widthElement = swipeElement.offsetWidth;
+  var axicX = axicX;  
+  
+
+  swipeBtnToShow.addEventListener('click', action);
+  
+  function action(elem){
+    if(swipeElement.style.transform){
+      swipeElement.style.transform = null;
+    }else{
+      swipeElement.style.transition = 'transform ' + delay +'s ease-out';
+      swipeElement.style.transform = 'translateX(' + axicX + widthElement + 'px)';
+    } 
+  }
 }
